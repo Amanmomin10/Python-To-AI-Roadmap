d1415822c2073bd4b359176f730657d442a4877e
Aman momin <127848274+Amanmomin10@users.noreply.github.com>
2025-11-17 12:53:52 +0530

    Add files via upload

---
+++ Python/Day-2/TypeConversion,Input.py
@@ -0,0 +1,17 @@
+#Type conversion is when we convert(cast) variables from one type to another.
+#1.Type conversion - Implicit, done automatically by Python.
+a = 5  
+b = 3.0  
+print(a + b) # Python converts answer in float by default
+
+#2.Type Casting - Explicitly, done by the programmer.
+x = 10        # int  
+y = float(x)  # convert to float 
+z = str(x)    # convert to string 
+print(x)
+print(y)
+print(z)
+
+#int(), float(), str(), bool(), list(), tuple() are common type conversion functions
+ 
+
