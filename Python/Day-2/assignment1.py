6ca2741b57373bd8ec1fd49a1403dcc718cf90ca
Aman momin <127848274+Amanmomin10@users.noreply.github.com>
2025-11-17 20:55:49 +0530

    Add files via upload

---
+++ Python/Day-2/assignment1.py
@@ -0,0 +1,57 @@
+'''
+Num1 = int(input("Enter number : "))
+Num2 = int(input("Enter number2 : "))
+Num3F = float(input("Enter number2 : "))
+d = float(Num1)
+e = float(Num2)
+print (d)
+print (e)
+print (Num3F)
+Avg = (Num1+Num2+Num3F)/3
+print (Avg)
+------------------------------------
+a = input("Enter number : ")
+x = int(a)
+y = float(a)
+z = str(a)
+print (type(x),x)
+print (type(y),y)
+print (type(z),z)
+------------------------------------
+x =3*2**2
+print (x)
+------------------------------------
+a = 2
+b = 4
+c = a 
+a = b
+b = c
+print("a:",a)
+print("b:",b)
+-------------------------------------
+a = input("Whats the temperature : ")
+Celsius = float(a)
+Fahrenhiet = (Celsius*(9/5))+32
+print (Celsius)
+print (Fahrenhiet)
+--------------------------------------
+r = float(input("Whats the Radius : "))
+π = 3.14
+Area = π*r**r
+print(Area)
+--------------------------------------
+P = float(input("Whats the Principle : "))
+R = float(input("Whats the Rate : "))
+T = float(input("Whats the Time : "))
+SI =  (P*R*T)/100
+print(SI)
+--------------------------------------
+Dn = float(input("What's the input : "))
+integer_part = int(Dn)
+fractional_part = round(Dn - integer_part, 2)
+
+print("Integer Number :", integer_part)
+print("Fractional Number : .", str(fractional_part)[2:])
+-------------------------------------------
+'''
+
